f5d58e4b5d7f9d317d1fb3ff406807cca179ab86
而已 <13423228+tzyhxm123@user.noreply.gitee.com>
2024-12-23 19:14:46 +0800

    1

--- refe/src/component/Firstshow/index.js
+++ refe/src/component/Firstshow/index.js
@@ -2,11 +2,11 @@ import './index.css'
 const FirstShow = () => {
     return (
         <div className='main-show'>
-            <h1 className='item item1'>HEllO W0rlD!</h1>
-            <h1 className='item item2'>HEllO W0rlD!</h1>
-            <h1 className='item item3'>HEllO W0rlD!</h1>
-            <h1 className='item item4'>HEllO W0rlD!</h1>
-            <h1 className='item item5'>HEllO W0rlD!</h1>
+            <i><h1 className='item item1'>HEllO W0rlD!</h1></i>
+            <i><h1 className='item item2'>HEllO W0rlD!</h1></i>
+            <i><h1 className='item item3'>HEllO W0rlD!</h1></i>
+            <i><h1 className='item item4'>HEllO W0rlD!</h1></i>
+            <i><h1 className='item item5'>HEllO W0rlD!</h1></i>
             {/* <div className='box box1'></div>
             <div className='box box2'></div>
             <div className='box box3'></div> */}
